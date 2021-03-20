2cf91deff475e7a6bc5ae745bee743333f5ef84f
Anshul <anshul.jaiswal2206@gmail.com>
2021-03-20 13:22:00 +0530

    Add project files.

---
+++ Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication9.Models
+{
+    public class Cart
+    {
+       [Key]
+        public int Id { get; set; }
+       public int MenuItemId { get; set; }
+        public MenuItem MenuItem { get; set; }
+    }
+}
---
+++ Models/MenuItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication9.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        public int Price { get; set; }
+        public bool Active { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOflaunch { get; set; }
+        [Display(Name="FreeDelivery")]
+        public bool FreeDelivery { get; set; }
+        
+        public int CategoryId { get; set; }
+        
+        public Category Category { get; set; }
+    }  
+   }
+
